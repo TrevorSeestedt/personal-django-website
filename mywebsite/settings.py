6b89c57d8482f0d3ed9ba4e3cbf4a624837b3538
Trevor <seestedt@email.sc.edu>
2025-01-29 16:14:05 -0500

    update

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = [
     'trevorseestedt.me',
     'www.trevorseestedt.me',
-    '*.up.railway.app',
+    'personal-website.up.railway.app',
     'localhost',
     '127.0.0.1',
 ]
