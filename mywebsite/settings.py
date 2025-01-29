b4cb2ee0d94eb54315f71521710759a63ec25d84
Trevor <seestedt@email.sc.edu>
2025-01-29 16:29:20 -0500

    update

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -33,7 +33,7 @@ DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = [
     'trevorseestedt.me',
     'www.trevorseestedt.me',
-    'pacific-empathy.up.railway.app',
+    'courteous-stillness.up.railway.app',
     'localhost',
     '127.0.0.1',
 ]
