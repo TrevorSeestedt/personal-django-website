46da919d3a4c13d05611de3d8153596178d2400b
Trevor <seestedt@email.sc.edu>
2025-02-06 19:36:49 -0500

    fix csrf settings

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -38,6 +38,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
+CSRF_TRUSTED_ORIGINS =  [
+    'https://personal-django-website-production.up.railway.app',
+    'https://www.trevorseestedt.me',
+]
 # Application definition
 
 INSTALLED_APPS = [
