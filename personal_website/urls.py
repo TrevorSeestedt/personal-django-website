ad22527e9f9bf5c245e1e603de848f15492d5139
Trevor <seestedt@email.sc.edu>
2025-02-06 19:55:59 -0500

    pragmatically adding a super user again

--- personal_website/urls.py
+++ personal_website/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views 
 
+from .views import create_admin_user
+
 urlpatterns = [
     path('', views.home_view, name='home'), 
     path('projects/', views.projects_view, name='projects'),
+
+    path('create-superuser/', create_admin_user),
 ]
--- personal_website/views.py
+++ personal_website/views.py
@@ -11,3 +11,12 @@ def projects_view(request):
         'projects': projects
     }
     return render(request, 'personal_website/projects.html', context)
+
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_admin_user(request):
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("trevorseestedt", "admin@example.com", "Trevor2002!")
+        return HttpResponse("Superuser created successfully.")
+    return HttpResponse("Superuser already exists.")
