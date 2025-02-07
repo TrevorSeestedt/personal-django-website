3c8d2854ba2f075303799ebba5ddc377ce7b215e
Trevor <seestedt@email.sc.edu>
2025-02-06 19:17:51 -0500

    creating a superuser as the Railway CLI is not working.

--- personal_website/urls.py
+++ personal_website/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home_view, name='home'), 
     path('projects/', views.projects_view, name='projects'),
 
+    # delete this after deployed. 
+    path('create-superuser/', views.create_admin)
 ]
