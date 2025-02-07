d48bb0373e1e301b9a3d5f5564144e7041de7977
Trevor <seestedt@email.sc.edu>
2025-02-06 19:58:26 -0500

    removed the pragmatic superuser creation

--- personal_website/urls.py
+++ personal_website/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from . import views 
 
-from .views import create_admin_user
-
 urlpatterns = [
     path('', views.home_view, name='home'), 
     path('projects/', views.projects_view, name='projects'),
-
-    path('create-superuser/', create_admin_user),
 ]
