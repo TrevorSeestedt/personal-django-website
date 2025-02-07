f05389b877368760c669cb36d58efac83784b395
Trevor <seestedt@email.sc.edu>
2025-02-06 19:23:23 -0500

    deleted the superuser creator view

--- personal_website/urls.py
+++ personal_website/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home'), 
     path('projects/', views.projects_view, name='projects'),
-
-    # delete this after deployed. 
-    path('create-superuser/', views.create_admin)
 ]
--- personal_website/views.py
+++ personal_website/views.py
@@ -11,13 +11,3 @@ def projects_view(request):
         'projects': projects
     }
     return render(request, 'personal_website/projects.html', context)
-
-
-# delete this after deployed  
-from django.contrib.auth.models import User 
-from django.http import HttpResponse
-def create_admin(request): 
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("steele2002", "admin@example.com", "8iA3y6@<otOr")
-        return HttpResponse("Superuser created successfully.")
-    return HttpResponse("Superuser already exists.")
